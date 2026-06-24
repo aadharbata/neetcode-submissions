936c8c82efd094494d718211028748eb60efb775
Aadhar Batra <138890041+aadharbata@users.noreply.github.com>
2026-06-25 00:36:55 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int i = n - 1;
+
+        for(int j = n - 2; j >= 0; j--){
+
+            if(j + nums[j] >= i){
+                i = j;
+            }
+        }
+
+        return i == 0;
+    }
+};
